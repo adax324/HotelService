6b9f0f2863c53c75cb88c8b32cc232380eb72aa2
Adaxator <adax324@gmail.com>
2021-05-16 15:47:25 +0200

    Initial commit

---
+++ src/main/java/com/hotelservice/App.java
@@ -0,0 +1,13 @@
+package com.hotelservice;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
